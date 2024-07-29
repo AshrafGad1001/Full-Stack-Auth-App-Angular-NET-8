4c1d3a1092cdff2baa6271c09ade239de32c02c2
Ashraf Gad <158779399+AshrafGad1001@users.noreply.github.com>
2024-07-29 21:10:03 +0300

    Login EndPoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,7 +1,11 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using API.DTOs;
 using API.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
 
 namespace API.Controllers
@@ -73,5 +77,96 @@ namespace API.Controllers
 
 
 
+        /*
+            - api/account/login
+         */
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> login(LoginDto loginDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user is null)
+            {
+                return Unauthorized(new AuthResponseDto
+                {
+                    IsSuccess = false,
+                    Message = "User Not Found With This Email"
+                });
+            }
+            var Result = await _userManager.CheckPasswordAsync(user, loginDto.Password);
+
+            if (!Result)
+            {
+                return Unauthorized(new AuthResponseDto
+                {
+                    IsSuccess = false,
+                    Message = "Invalid Password"
+                });
+            }
+
+            var token = GenerateToken(user);
+            return Ok(new AuthResponseDto
+            {
+                Token = token,
+                IsSuccess = true,
+                Message = "Login Success."
+            });
+
+
+        }
+
+
+
+
+
+
+
+        /*
+        -Function To Generate Token
+        */
+
+        private string GenerateToken(AppUser user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes
+                        (_configuration.GetSection("JWTSetting").GetSection("securityKey").Value!);
+
+            var roles = _userManager.GetRolesAsync(user).Result;
+
+            List<Claim> claims =
+            [
+                new (JwtRegisteredClaimNames.Email,user.Email??""),
+                new (JwtRegisteredClaimNames.Name,user.FullName??""),
+                new (JwtRegisteredClaimNames.NameId,user.Id??""),
+                new (JwtRegisteredClaimNames.Aud,
+                    _configuration.GetSection("JWTSetting").GetSection("ValidAudience").Value!),
+                new (JwtRegisteredClaimNames.Iss,
+                    _configuration.GetSection("JWTSetting").GetSection("ValidIssuer").Value!)
+
+            ];
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(30),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256
+                )
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
     }
 }
