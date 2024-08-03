23b5cf1e10312720fad35af6abb2bb54078f79c2
Ashraf Gad <158779399+AshrafGad1001@users.noreply.github.com>
2024-08-03 10:56:26 +0300

    RegisterForm UI

--- Client/src/app/pages/register/register.component.ts
+++ Client/src/app/pages/register/register.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSelectModule } from '@angular/material/select';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports:
+    [
+      MatInputModule,
+      MatIconModule,
+      MatSelectModule,
+      ReactiveFormsModule,
+      RouterLink,
+      MatSnackBarModule
+    ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
