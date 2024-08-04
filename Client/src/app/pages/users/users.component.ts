bd776c7dbf6894d2db72524f491311bc71c0744e
Ashraf Gad <158779399+AshrafGad1001@users.noreply.github.com>
2024-08-04 20:17:55 +0300

    Users

--- Client/src/app/pages/users/users.component.ts
+++ Client/src/app/pages/users/users.component.ts
@@ -1,13 +1,19 @@
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { AuthService } from './../../services/auth.service';
 import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports:
+    [
+      CommonModule,
+      AsyncPipe
+    ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
 export class UsersComponent {
   authService = inject(AuthService);
+  users$ = this.authService.getAll();
 }
