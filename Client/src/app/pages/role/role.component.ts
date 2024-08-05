7d72842f8ac0608d308ca95c146de19ba20c9991
Ashraf Gad <158779399+AshrafGad1001@users.noreply.github.com>
2024-08-06 02:34:58 +0300

    Add Role Done

--- Client/src/app/pages/role/role.component.ts
+++ Client/src/app/pages/role/role.component.ts
@@ -2,6 +2,9 @@ import { Component, inject } from '@angular/core';
 import { RoleFormComponent } from "../../components/role-form/role-form.component";
 import { RoleService } from '../../services/role.service';
 import { RoleCreateRequest } from '../../Interfaces/role-create-request';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-role',
@@ -14,11 +17,26 @@ import { RoleCreateRequest } from '../../Interfaces/role-create-request';
 })
 export class RoleComponent {
   roleService = inject(RoleService)
-  errorMessage = '';
+  errorMessage: string = '';
   role: RoleCreateRequest = {} as RoleCreateRequest;
+  matSnackBar = inject(MatSnackBar);
+  router = inject(Router);
 
 
   createRole(role: RoleCreateRequest) {
+    console.log(this.errorMessage.toString());
+    this.roleService.createRole(role).subscribe({
 
+      next: (response: { message: string }) => {
+        this.matSnackBar.open('Role Created Successfully.', "Ok", {
+          duration: 5000,
+
+        });
+        this.router.navigate(['/']);
+      },
+      error: (error: HttpErrorResponse) => {
+        this.errorMessage = error.error;
+      }
+    });
   }
 }
--- Client/src/app/services/role.service.ts
+++ Client/src/app/services/role.service.ts
@@ -3,6 +3,7 @@ import { environment } from '../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Role } from '../Interfaces/role'
+import { RoleCreateRequest } from '../Interfaces/role-create-request';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,5 +13,10 @@ export class RoleService {
 
   getRoles = (): Observable<Role[]> => {
     return this.http.get<Role[]>(`${this.apiUrl}roles`);
+  }
+
+
+  createRole = (role: RoleCreateRequest): Observable<{ message: string }> => {
+    return this.http.post<{ message: string; }>(`${this.apiUrl}roles`, role);
   }
 }
